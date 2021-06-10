b3d6ba16b5b4538fc3774f17541e949d74b98b3c
cyjseagull <yujiechen_hust@163.com>
2021-06-10 10:07:40 +0800

    add install cmake scripts for bcos-front and bcos-gateway

--- InstallBcosStorageDependencies.cmake
+++ InstallBcosStorageDependencies.cmake
@@ -1,7 +1,8 @@
 hunter_add_package(bcos-storage)
 find_package(bcos-framework CONFIG REQUIRED)
 find_package(zstd CONFIG REQUIRED)
-find_package(RocksDB CONFIG REQUIRED)]
+find_package(RocksDB CONFIG REQUIRED)
+find_package(Boost CONFIG REQUIRED serialization)
 find_package(bcos-storage CONFIG REQUIRED)
 get_target_property(BCOS_STORAGE_INCLUDE bcos-storage::bcos-storage INTERFACE_INCLUDE_DIRECTORIES)
 include_directories(${BCOS_STORAGE_INCLUDE}/bcos-storage)
